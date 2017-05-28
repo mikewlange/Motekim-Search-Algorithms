5b55eca125e3182d059aeb3ab2b566004daedd14
mikewlange <mikewlange@gmail.com>
2017-05-28 01:13:25 -0400

    Initial Commit

---
+++ MotekimSearchAlgos/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MotekimSearchAlgos
+//
+//  Created by Michael Lange on 5/28/17.
+//  Copyright © 2017 motekim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MotekimSearchAlgos/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MotekimSearchAlgos
+//
+//  Created by Michael Lange on 5/28/17.
+//  Copyright © 2017 motekim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
